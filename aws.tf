4684e460ccd6ceac6a19b99634f759d7e344264d
Ankit Soni <58499741+ankitsoni570@users.noreply.github.com>
2020-02-29 09:27:13 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAU4VT5HACWLL4ZZLT"
-  secret_key = "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"
+  access_key = "<ACCESS_KEY_FROM_AWS>"
+  secret_key = "SECRET_FROM_AWS"
   region = "us-east-1"
 }
 
